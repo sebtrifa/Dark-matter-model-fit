23b8c0b88e18c1c1187a5ba24d90c50a10c1f624
Sebastian Trifa st14136.2014@my.bristol.ac.uk <st14136@soolin.dice.priv>
2018-03-05 03:38:28 +0000

    04/03/18

---
+++ flush2_myinputs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+i=$(sed -n "1p" /storage/st14136/zprime-test/MG5_aMC_v2_6_1/i.sh)
+a=$(sed -n "$i p" /storage/st14136/zprime-test/MG5_aMC_v2_6_1/my.sh)
+b=$(sed -n "$i p" /storage/st14136/zprime-test/MG5_aMC_v2_6_1/mx.sh)
+c=$(sed -n "$i p" /storage/st14136/zprime-test/MG5_aMC_v2_6_1/gx.sh)
+d=$(sed -n "$i p" /storage/st14136/zprime-test/MG5_aMC_v2_6_1/gq.sh)
+cat > /storage/st14136/zprime-test/MG5_aMC_v2_6_1/myinputs.dat <<EOF
+import model DMsimp_s_spin1
+generate p p > xd xd~ j [QCD]
+launch $a-$b-$d-$c
+1
+0
+EOF
+
